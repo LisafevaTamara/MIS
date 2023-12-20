1fd09573b70bba4ffac5ae37e76ead43b78bb0ff
MaximKorolyov <max_klasikl@mail.ru>
2023-12-21 02:20:00 +0300

    db_migr

--- db/add_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE public.my_table (
-	FIO TEXT,
-        Date DATE,
-        Address TEXT,
-        Doc_FIO TEXT,
-        Symptoms TEXT,
-        Drug_title TEXT,
-        diagnosis TEXT	
-);
